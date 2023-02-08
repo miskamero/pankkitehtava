1bf3db32713052a438e8cc8d3bfb6932181e0b94
miskamero <miskamero@users.noreply.github.com>
2023-02-08 08:27:33 +0200

    Olio 6 tehty

--- pankkiskripti.js
+++ pankkiskripti.js
@@ -1,18 +1,21 @@
 var rahaMaara;
 var IBAN;
-var tarkkailu = new RegExp(/[A-Z]{2}\d{2} ?\d{4} ?\d{4} ?\d{4} ?\d{2} ?[\d]{0,2}/gm)
+var tarkkailu = new RegExp(/[A-Z]{2}\d{2} ?\d{4} ?\d{4} ?\d{4} ?\d{2} ?[\d]{0,2}/gm);
+var nettoTulot;
+var luotto;
 // let date = new Date();
 // console.log(date)
 
 function kysyTili() {
-  IBAN = prompt("Kirjoita tilinumerosi, example tilinumero on: FI23 8723 2987 8732 23")
+  IBAN = prompt("Kirjoita tilinumerosi, example tilinumero on: FI23 8723 2987 8732 23");
 }
 
 class pankki {
-    constructor(tilinumero, saldo = 0, historia = []) {
+    constructor(tilinumero, saldo = 0, historia = [], korko = 0.02) {
       this.tilinumero = tilinumero;
       this.saldo = saldo;
       this.historia = historia;
+      this.korko = korko;
     }
     talleta() {
         rahaMaara = parseInt(prompt("Kuinka paljon talletetaan rahaa?"));
@@ -32,11 +35,15 @@ class pankki {
     }
     naytaTiedot() {
       document.getElementById("tietoja").innerHTML =
-      "Tilisi tiedot ovat:<br>" + "Tilinumero: " + IBAN + "<br>Saldo: " + this.saldo + " €";
+      "Tilisi tiedot ovat:<br>" + "Tilinumero: " + IBAN + "<br>Saldo: " + this.saldo.toFixed(2) + " €" +
+      "<br>Mahdollinen luottosi: " + luotto + " €";
     }
     naytaHistoria() {
         document.getElementById("historia").innerHTML =
         "Tilisi historia: <br>" + this.historia.join("<br>");
+    }
+    korkoFunktio() {
+      this.saldo += this.saldo * this.korko
     }
 }
 class uusiPankki extends pankki {
@@ -50,14 +57,16 @@ class uusiPankki extends pankki {
       let date = new Date();
       if (rahaMaara <= this.saldo) {
         this.saldo -= rahaMaara;
-        this.historia.push(date + " Nosto " + rahaMaara + "€" + " Saldo ennen tapahtumaa: " + this.saldo + "€" + "<br>");
+        this.historia.push(date + " Nosto " + rahaMaara + "€" + " Saldo tapahtuman jälkeen: " + this.saldo + "€" + "<br>");
       }else {
         alert("Tililläsi ei ole tarpeeksi katetta.")
       }
     }
     tarkistaIBAN() {
       if (IBAN.match(tarkkailu)) {
-        console.log("We are in..")
+        console.log("We are in..");
+        nettoTulot = parseInt(prompt("Kerro nettotulosi (€/kk)"));
+        luotto = nettoTulot * 0.25;
       } else {
         alert("Viallinen tilinumero!")
         location.reload()
@@ -69,8 +78,8 @@ let tili2 = new uusiPankki(IBAN);
 console.log("Muokkaus :)")
 
 
-//Lisää uuteen luokkaan metodi, joka tarkistaa, onko tilillä tarpeeksi rahaa tietyn summan nostamiseen. DONE
-//Voit esimerkiksi palauttaa arvon true tai false riippuen siitä, onko tilillä tarpeeksi rahaa. DONE
-//Tilinumero, jolta rahaa ollaan nostamassa kysytään käyttäjältä. DONE
+//Lisää korko: Lisää tilille korkoa kasvattava metodi. DONE
 
-//Lisää uuteen luokkaan myös metodi, joka tarkistaa tilinumeron validiuden (esim. tilinumeron pituuden tai tietyt numerosarjat). DONE
+//Lisää luotonsaantimahdollisuus: 
+//Lisää luottorajan tarkistava metodi, jolla pelaaja voi saada luottoa. 
+//Luottorajaan vaikuttaa käyttäjän nettotulot, joka kysytään käyttäjältä. Maksimi luottoraja on 25% kk nettotuloista.  
